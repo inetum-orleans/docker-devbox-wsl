b5a2008f6946a902827edd675efe8e102afbea3c
Vincent Ostyn <vincent.ostyn@inetum.com>
2024-03-22 17:28:57 +0100

    feat(install): check if the script is run with sudo

--- install.sh
+++ install.sh
@@ -1,47 +1,53 @@
 # INSTALL DDB ON WSL WITH AN OFFICIAL UBUNTU VERSION
 
+# Check if the script is run as root if not rerun as root
+if [ "$(id -u)" != "0" ]; then
+   sudo $0
+    exit $?
+fi
+
 #Update and upgrade packages
-sudo apt-get update -y
-sudo apt-get upgrade -y
+apt-get update -y
+apt-get upgrade -y
 
 #Install and configuration of Docker
 ## Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get update
+apt-get install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 ## Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ## By default, the Docker daemon requires root privileges to run. To use Docker without sudo, add your user to the Docker group
-sudo usermod -aG docker $USER
+usermod -aG docker $USER
 
 ## Change permissions of the Docker socket
-sudo chmod 666 /var/run/docker.sock
+chmod 666 /var/run/docker.sock
 
 ## Expose docker to Windows by 2375 port create as root
-sudo mkdir -p /etc/systemd/system/docker.service.d
+mkdir -p /etc/systemd/system/docker.service.d
 
 ## Create override.conf file to expose docker to Windows
-echo -e "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock" | sudo tee /etc/systemd/system/docker.service.d/override.conf > /dev/null
+echo -e "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock" | tee /etc/systemd/system/docker.service.d/override.conf > /dev/null
 
 
 ## Create daemon.json file to set default address pool
-echo -e "{\n \"default-address-pools\": [{\"base\":\"10.199.0.0/16\", \"size\":24}]}" | sudo tee /etc/docker/daemon.json > /dev/null
+echo -e "{\n \"default-address-pools\": [{\"base\":\"10.199.0.0/16\", \"size\":24}]}" | tee /etc/docker/daemon.json > /dev/null
 
 ## Reload the Docker daemon
-sudo systemctl daemon-reload
+systemctl daemon-reload
 ## Restart Docker
-sudo systemctl restart docker
+systemctl restart docker
 
 # Install and configuration of Docker Compose
 DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
