11ccaefa5e42bcc02c46235fbc0679ea4ee08328
Jérémy VIGNELLES <jeremy.vignelles@inetum.com>
2025-04-17 15:10:25 +0200

    chore(install): do not expose docker daemon

--- install.sh
+++ install.sh
@@ -38,40 +38,6 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docke
 ## By default, the Docker daemon requires root privileges to run. To use Docker without sudo, add your user to the Docker group
 usermod -aG docker $DDB_USER
 
-## Expose docker to Windows by 2375 port create as root
-mkdir -p /etc/systemd/system/docker.service.d
-
-## Create override.conf file to expose docker to Windows
-### Define the string to check and add
-EXPOSE_DOCKER="[Service]\nExecStart=\nExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock"
-### Define the path to the override.conf file
-OVERRIDE_PATH="/etc/systemd/system/docker.service.d/override.conf"
-
-### Check if the file exist or the string exists in the override.conf file
-if [ ! -f "$OVERRIDE_PATH" ] || ! grep -Fxq "$EXPOSE_DOCKER" "$OVERRIDE_PATH"
-then
-    ### If the string does not exist, append it to the file
-    echo -e "$EXPOSE_DOCKER" >> "$OVERRIDE_PATH"
-fi
-
-## Create daemon.json file to set default address pool
-### Define the string to check and add
-ADDRESS_POOL="{\n \"default-address-pools\": [{\"base\":\"10.199.0.0/16\", \"size\":24}]\n}"
-### Define the path to the daemon.json file
-DAEMON_PATH="/etc/docker/daemon.json"
-
-### Check if the file exist or the string exists in the daemon.json file
-if [ ! -f "$DAEMON_PATH" ] || ! grep -Fxq "$ADDRESS_POOL" "$DAEMON_PATH"
-then
-    ### If the string does not exist, append it to the file
-    echo -e "$ADDRESS_POOL" >> "$DAEMON_PATH"
-fi
-
-## Reload the Docker daemon
-systemctl daemon-reload
-## Restart Docker
-systemctl restart docker
-
 # Check if Docker service is running
 for i in {1..30}
 do
@@ -95,12 +61,8 @@ fi
 # Add the HOST_IP variable to the .bashrc file
 echo 'export HOST_IP=$(ip route show default | awk '\''{print $3}'\'')' >> /home/$DDB_USER/.bashrc
 
-# Install DDB
-su -c 'curl -L https://github.com/inetum-orleans/docker-devbox/raw/master/installer | bash' - $DDB_USER
-
-
 # Configure the global ddb.yml file
-cat <<EOF > "/home/$DDB_USER/.docker-devbox/ddb.yaml"
+cat <<EOF > "/home/$DDB_USER/ddb.yaml"
 # =======================================================================
 # Generated file by inetum-orleans/docker-devbox-wsl on $(date +"%Y/%m/%d")
 # Do not modify. To override, create a ddb.local.yaml file.
@@ -114,5 +76,8 @@ shell:
   global_aliases:
     - dc
 EOF
+
+# Install DDB
+su -c 'curl -L https://github.com/inetum-orleans/docker-devbox/raw/master/installer | bash' - $DDB_USER
 
 echo 'Please restart your terminal to apply the changes.'
